2775590e7e2f95b150f18936374cb25e42203ede
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-03-16 11:14:50 +0100

    acceptance: run via travis
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- exoscale/provider_test.go
+++ exoscale/provider_test.go
@@ -11,6 +11,13 @@ import (
 var testAccProviders map[string]terraform.ResourceProvider
 var testAccProvider *schema.Provider
 
+func init() {
+	testAccProvider = Provider().(*schema.Provider)
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"exoscale": testAccProvider,
+	}
+}
+
 func TestProvider(t *testing.T) {
 	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
