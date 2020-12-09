59bbfb2f93b4db6877a03ef29b4607312e00de17
Marc Falzon <marc@exoscale.com>
2020-12-09 08:52:07 +0100

    tests: update reference template IDs

--- exoscale/provider_test.go
+++ exoscale/provider_test.go
@@ -18,14 +18,16 @@ const (
 	testDescription          = "Created by the terraform-exoscale provider"
 	testZoneName             = "ch-gva-2"
 	testInstanceTemplateName = "Linux Ubuntu 20.04 LTS 64-bit"
-	testInstanceTemplateID   = "c19542b7-d269-4bd4-bf7c-2cae36d066d3" // "Linux Ubuntu 20.04 LTS 64-bit" @ ch-gva-2
+	testInstanceTemplateID   = "806adb6b-a35b-4033-b000-8bec1b986c8e" // "Linux Ubuntu 20.04 LTS 64-bit" @ ch-gva-2
 )
 
 // testAttrs represents a map of expected resource attributes during acceptance tests.
 type testAttrs map[string]schema.SchemaValidateFunc
 
-var testAccProviders map[string]terraform.ResourceProvider
-var testAccProvider *schema.Provider
+var (
+	testAccProviders map[string]terraform.ResourceProvider
+	testAccProvider  *schema.Provider
+)
 
 var testEnvironment string
 
