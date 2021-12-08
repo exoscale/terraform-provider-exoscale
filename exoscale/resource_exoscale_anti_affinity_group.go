b78e8835167ab2d82df86a300aa62e9dc5e6703f
Marc Falzon <marc@exoscale.com>
2021-12-08 11:16:39 +0100

    Add new resource "exoscale_anti_affinity_group"
    This change introduces a new resource `exoscale_anti_affinity_group`
    using the new Exocale public API; this deprecates the
    `exoscale_affinity` resource.

---
+++ exoscale/resource_exoscale_anti_affinity_group.go
@@ -0,0 +1,141 @@
+package exoscale
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	egoscale "github.com/exoscale/egoscale/v2"
+	exoapi "github.com/exoscale/egoscale/v2/api"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+const (
+	resAntiAffinityGroupAttrDescription = "description"
+	resAntiAffinityGroupAttrName        = "name"
+)
+
+func resourceAntiAffinityGroupIDString(d resourceIDStringer) string {
+	return resourceIDString(d, "exoscale_anti_affinity_group")
+}
+
+func resourceAntiAffinityGroup() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			resAntiAffinityGroupAttrDescription: {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			resAntiAffinityGroupAttrName: {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+
+		CreateContext: resourceAntiAffinityGroupCreate,
+		ReadContext:   resourceAntiAffinityGroupRead,
+		DeleteContext: resourceAntiAffinityGroupDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(defaultTimeout),
+			Read:   schema.DefaultTimeout(defaultTimeout),
+			Delete: schema.DefaultTimeout(defaultTimeout),
+		},
+	}
+}
+
+func resourceAntiAffinityGroupCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	log.Printf("[DEBUG] %s: beginning create", resourceAntiAffinityGroupIDString(d))
+
+	zone := defaultZone
+
+	ctx, cancel := context.WithTimeout(ctx, d.Timeout(schema.TimeoutCreate))
+	ctx = exoapi.WithEndpoint(ctx, exoapi.NewReqEndpoint(getEnvironment(meta), zone))
+	defer cancel()
+
+	client := GetComputeClient(meta)
+
+	antiAffinityGroup, err := client.CreateAntiAffinityGroup(ctx, zone, &egoscale.AntiAffinityGroup{
+		Name:        nonEmptyStringPtr(d.Get(resAntiAffinityGroupAttrName).(string)),
+		Description: nonEmptyStringPtr(d.Get(resAntiAffinityGroupAttrDescription).(string)),
+	})
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(*antiAffinityGroup.ID)
+
+	log.Printf("[DEBUG] %s: create finished successfully", resourceAntiAffinityGroupIDString(d))
+
+	return resourceAntiAffinityGroupRead(ctx, d, meta)
+}
+
+func resourceAntiAffinityGroupRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	log.Printf("[DEBUG] %s: beginning read", resourceAntiAffinityGroupIDString(d))
+
+	zone := defaultZone
+
+	ctx, cancel := context.WithTimeout(ctx, d.Timeout(schema.TimeoutRead))
+	ctx = exoapi.WithEndpoint(ctx, exoapi.NewReqEndpoint(getEnvironment(meta), zone))
+	defer cancel()
+
+	client := GetComputeClient(meta)
+
+	antiAffinityGroup, err := client.GetAntiAffinityGroup(ctx, zone, d.Id())
+	if err != nil {
+		if errors.Is(err, exoapi.ErrNotFound) {
+			// Resource doesn't exist anymore, signaling the core to remove it from the state.
+			d.SetId("")
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+
+	log.Printf("[DEBUG] %s: read finished successfully", resourceAntiAffinityGroupIDString(d))
+
+	return diag.FromErr(resourceAntiAffinityGroupApply(ctx, d, antiAffinityGroup))
+}
+
+func resourceAntiAffinityGroupDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	log.Printf("[DEBUG] %s: beginning delete", resourceAntiAffinityGroupIDString(d))
+
+	zone := defaultZone
+
+	ctx, cancel := context.WithTimeout(ctx, d.Timeout(schema.TimeoutDelete))
+	ctx = exoapi.WithEndpoint(ctx, exoapi.NewReqEndpoint(getEnvironment(meta), zone))
+	defer cancel()
+
+	client := GetComputeClient(meta)
+
+	antiAffinityGroupID := d.Id()
+	if err := client.DeleteAntiAffinityGroup(ctx, zone, &egoscale.AntiAffinityGroup{ID: &antiAffinityGroupID}); err != nil {
+		return diag.FromErr(err)
+	}
+
+	log.Printf("[DEBUG] %s: delete finished successfully", resourceAntiAffinityGroupIDString(d))
+
+	return nil
+}
+
+func resourceAntiAffinityGroupApply(
+	_ context.Context,
+	d *schema.ResourceData,
+	antiAffinityGroup *egoscale.AntiAffinityGroup,
+) error {
+	if err := d.Set(resAntiAffinityGroupAttrName, *antiAffinityGroup.Name); err != nil {
+		return err
+	}
+
+	if err := d.Set(resAntiAffinityGroupAttrDescription, defaultString(antiAffinityGroup.Description, "")); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ website/docs/r/anti_affinity_group.html.markdown
@@ -0,0 +1,47 @@
+---
+layout: "exoscale"
+page_title: "Exoscale: exoscale_anti_affinity_group"
+sidebar_current: "docs-exoscale-anti-affinity-group"
+description: |-
+  Provides an Exoscale Anti-Affinity Group resource.
+---
+
+# exoscale\_affinity
+
+Provides an Exoscale [Anti-Affinity Group][aag-doc] resource. This can be used to create and delete Anti-Affinity Groups.
+
+
+## Example Usage
+
+```hcl
+resource "exoscale_anti_affinity_group" "example" {
+  name        = "cluster"
+  description = "HA Cluster"
+}
+```
+
+
+## Arguments Reference
+
+* `name` - (Required) The name of the Anti-Affinity Group.
+* `description` - An Anti-Affinity Group description.
+
+
+## Attributes Reference
+
+In addition to the arguments listed above, the following attributes are exported:
+
+* `id` - The ID of the Anti-Affinity Group.
+
+
+## Import
+
+An existing Anti-Affinity Group can be imported as a resource its ID:
+
+```console
+$ terraform import exoscale_anti_affinity_group.my-group eb556678-ec59-4be6-8c54-0406ae0f6da6
+```
+
+
+[aag-doc]: https://community.exoscale.com/documentation/compute/anti-affinity-groups/
+
