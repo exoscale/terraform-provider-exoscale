563fc1ba70dfc36016aabc9aab9a132ce464de29
Marc Falzon <marc@exoscale.com>
2021-12-08 19:15:24 +0100

    Add new data source "exoscale_private_network"
    This change introduces a new data source `exoscale_private_network`
    using the new Exocale public API; this deprecates the `exoscale_network`
    data source.

---
+++ exoscale/datasource_exoscale_private_network_test.go
@@ -0,0 +1,121 @@
+package exoscale
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+var (
+	testAccDataSourcePrivateNetworkDescription = acctest.RandString(10)
+	testAccDataSourcePrivateNetworkEndIP       = "10.0.0.50"
+	testAccDataSourcePrivateNetworkName        = acctest.RandString(10)
+	testAccDataSourcePrivateNetworkNetmask     = "255.255.0.0"
+	testAccDataSourcePrivateNetworkStartIP     = "10.0.0.10"
+	testAccDataSourcePrivateNetworkZone        = testZoneName
+)
+
+func TestAccDataSourcePrivateNetwork(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      `data "exoscale_private_network" "test" { zone = "" }`,
+				ExpectError: regexp.MustCompile("either name or id must be specified"),
+			},
+			{
+				Config: fmt.Sprintf(`
+resource "exoscale_private_network" "test" {
+  zone = "%s"
+  name = "%s"
+}
+
+data "exoscale_private_network" "by-id" {
+  zone = exoscale_private_network.test.zone
+  id   = exoscale_private_network.test.id
+}`,
+					testAccDataSourcePrivateNetworkZone,
+					testAccDataSourcePrivateNetworkName,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					testAccDataSourcePrivateNetworkAttributes("data.exoscale_private_network.by-id", testAttrs{
+						dsPrivateNetworkAttrID:   validation.ToDiagFunc(validation.IsUUID),
+						dsPrivateNetworkAttrName: validateString(testAccDataSourcePrivateNetworkName),
+					}),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+resource "exoscale_private_network" "test" {
+  zone = "%s"
+  name = "%s"
+}
+
+data "exoscale_private_network" "by-name" {
+  zone = exoscale_private_network.test.zone
+  name = exoscale_private_network.test.name
+}`,
+					testAccDataSourcePrivateNetworkZone,
+					testAccDataSourcePrivateNetworkName,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					testAccDataSourcePrivateNetworkAttributes("data.exoscale_private_network.by-name", testAttrs{
+						dsPrivateNetworkAttrID:   validation.ToDiagFunc(validation.IsUUID),
+						dsPrivateNetworkAttrName: validateString(testAccDataSourcePrivateNetworkName),
+					}),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+resource "exoscale_private_network" "test" {
+  zone        = "%s"
+  name        = "%s"
+  description = "%s"
+  start_ip    = "%s"
+  end_ip      = "%s"
+  netmask     = "%s"
+}
+
+data "exoscale_private_network" "test" {
+  zone = exoscale_private_network.test.zone
+  name = exoscale_private_network.test.name
+}`,
+					testAccDataSourcePrivateNetworkZone,
+					testAccDataSourcePrivateNetworkName,
+					testAccDataSourcePrivateNetworkDescription,
+					testAccDataSourcePrivateNetworkStartIP,
+					testAccDataSourcePrivateNetworkEndIP,
+					testAccDataSourcePrivateNetworkNetmask,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					testAccDataSourceAntiAffinityGroupAttributes("data.exoscale_private_network.test", testAttrs{
+						dsPrivateNetworkAttrDescription: validateString(testAccDataSourcePrivateNetworkDescription),
+						dsPrivateNetworkAttrEndIP:       validateString(testAccDataSourcePrivateNetworkEndIP),
+						dsPrivateNetworkAttrName:        validateString(testAccDataSourcePrivateNetworkName),
+						dsPrivateNetworkAttrNetmask:     validateString(testAccDataSourcePrivateNetworkNetmask),
+						dsPrivateNetworkAttrStartIP:     validateString(testAccDataSourcePrivateNetworkStartIP),
+					}),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourcePrivateNetworkAttributes(ds string, expected testAttrs) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		for name, res := range s.RootModule().Resources {
+			if name == ds {
+				return checkResourceAttributes(expected, res.Primary.Attributes)
+			}
+		}
+
+		return errors.New("exoscale_private_network data source not found in the state")
+	}
+}
