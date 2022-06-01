960c58dafb25ca318403791a8a04e59a8903d921
Predrag Janosevic <predrag.janosevic@exoscale.com>
2022-06-01 14:08:46 +0200

    New data source "exoscale_compute_instance_list" (#178)
    * Add new data source "exoscale_compute_instance_list"
    
    * datasource instance list: update docs

---
+++ exoscale/datasource_exoscale_compute_instance_list.go
@@ -0,0 +1,119 @@
+package exoscale
+
+import (
+	"context"
+	"crypto/md5"
+	"fmt"
+	"log"
+	"sort"
+	"strings"
+
+	exoapi "github.com/exoscale/egoscale/v2/api"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceComputeInstanceList() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			dsComputeInstanceAttrZone: {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"instances": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: getDataSourceComputeInstanceSchema(),
+				},
+			},
+		},
+
+		ReadContext: dataSourceComputeInstanceListRead,
+	}
+}
+
+func dataSourceComputeInstanceListRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	log.Printf("[DEBUG] %s: beginning read", resourceIDString(d, "exoscale_compute_instance_list"))
+
+	zone := d.Get(dsComputeInstanceAttrZone).(string)
+
+	ctx, cancel := context.WithTimeout(ctx, d.Timeout(schema.TimeoutRead))
+	ctx = exoapi.WithEndpoint(ctx, exoapi.NewReqEndpoint(getEnvironment(meta), zone))
+	defer cancel()
+
+	client := GetComputeClient(meta)
+
+	instances, err := client.ListInstances(
+		ctx,
+		zone,
+	)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	data := make([]interface{}, 0, len(instances))
+	ids := make([]string, 0, len(instances))
+	instanceTypes := map[string]string{}
+
+	for _, item := range instances {
+		// we use ID to generate a resource ID, we cannot list instances without ID.
+		if item.ID == nil {
+			continue
+		}
+
+		ids = append(ids, *item.ID)
+
+		instance, err := client.FindInstance(
+			ctx,
+			zone,
+			*item.ID,
+		)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		instanceData, err := dataSourceComputeInstanceBuildData(instance)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		if instance.InstanceTypeID != nil {
+			tid := *instance.InstanceTypeID
+			if _, ok := instanceTypes[tid]; !ok {
+				instanceType, err := client.GetInstanceType(
+					ctx,
+					zone,
+					tid,
+				)
+				if err != nil {
+					return diag.Errorf("unable to retrieve instance type: %s", err)
+				}
+				instanceTypes[tid] = fmt.Sprintf(
+					"%s.%s",
+					strings.ToLower(*instanceType.Family),
+					strings.ToLower(*instanceType.Size),
+				)
+			}
+
+			instanceData[dsComputeInstanceAttrType] = instanceTypes[tid]
+		}
+
+		data = append(data, instanceData)
+	}
+
+	err = d.Set("instances", data)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// by sorting instance IDs we can generate the same resource ID regardless of the order in which
+	// API returns instances in thelist.
+	sort.Strings(ids)
+
+	d.SetId(fmt.Sprintf("%x", md5.Sum([]byte(strings.Join(ids, "")))))
+
+	log.Printf("[DEBUG] %s: read finished successfully", resourceIDString(d, "exoscale_compute_instance_list"))
+
+	return nil
+}
