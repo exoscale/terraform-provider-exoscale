dc3daea88e9d93a81867e064a5207257551cc72c
Philippe Chepy <philippe.chepy@exoscale.ch>
2022-06-21 11:25:15 +0200

    Add acceptance tests for iam_access_key

---
+++ exoscale/resource_exoscale_iam_access_key_test.go
@@ -0,0 +1,109 @@
+package exoscale
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+
+	egoscale "github.com/exoscale/egoscale/v2"
+	exoapi "github.com/exoscale/egoscale/v2/api"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	testAccResourceIAMAccessKeyName = acctest.RandomWithPrefix(testPrefix)
+
+	testAccResourceIAMAccessKeyConfigCreate = fmt.Sprintf(`
+resource "exoscale_iam_access_key" "test" {
+  name       = "%s"
+
+  operations = ["list-instances"]
+  tags       = ["sos"]
+}
+`,
+		testAccResourceIAMAccessKeyName,
+	)
+)
+
+func TestAccResourceIAMAccessKey(t *testing.T) {
+	var (
+		r            = "exoscale_iam_access_key.test"
+		iamAccessKey egoscale.IAMAccessKey
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckResourceIAMAccessKeyDestroy(&iamAccessKey),
+		Steps: []resource.TestStep{
+			{
+				// Create
+				Config: testAccResourceIAMAccessKeyConfigCreate,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceIAMAccessKeyExists(r, &iamAccessKey),
+					func(s *terraform.State) error {
+						a := assert.New(t)
+						a.Equal(testAccResourceIAMAccessKeyName, *iamAccessKey.Name)
+						return nil
+					},
+					checkResourceState(r, checkResourceStateValidateAttributes(testAttrs{
+						resIAMAccessKeyAttrKey:  validation.ToDiagFunc(validation.NoZeroValues),
+						resIAMAccessKeyAttrName: validateString(testAccResourceIAMAccessKeyName),
+						/*						resIAMAccessKeyAttrOperations:
+												resIAMAccessKeyAttrResources:*/
+						resIAMAccessKeyAttrSecret: validation.ToDiagFunc(validation.NoZeroValues),
+						/*						resIAMAccessKeyAttrLabels:*/
+
+					})),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckResourceIAMAccessKeyExists(r string, iamAccessKey *egoscale.IAMAccessKey) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[r]
+		if !ok {
+			return errors.New("resource not found in the state")
+		}
+
+		if rs.Primary.ID == "" {
+			return errors.New("resource ID not set")
+		}
+
+		client := GetComputeClient(testAccProvider.Meta())
+
+		ctx := exoapi.WithEndpoint(context.Background(), exoapi.NewReqEndpoint(testEnvironment, testZoneName))
+		res, err := client.GetIAMAccessKey(ctx, testZoneName, rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		*iamAccessKey = *res
+		return nil
+	}
+}
+
+func testAccCheckResourceIAMAccessKeyDestroy(iamAccessKey *egoscale.IAMAccessKey) resource.TestCheckFunc {
+	return func(_ *terraform.State) error {
+		client := GetComputeClient(testAccProvider.Meta())
+		ctx := exoapi.WithEndpoint(context.Background(), exoapi.NewReqEndpoint(testEnvironment, testZoneName))
+
+		_, err := client.GetIAMAccessKey(ctx, testZoneName, *iamAccessKey.Key)
+		if err != nil {
+			if errors.Is(err, exoapi.ErrNotFound) {
+				return nil
+			}
+
+			return err
+		}
+
+		return errors.New("SSH Key still exists")
+	}
+}
