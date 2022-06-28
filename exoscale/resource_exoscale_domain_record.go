f3e3a046bb2c87ec98961aceb610fc06a2298dfa
Predrag Janosevic <predrag.janosevic@exoscale.com>
2022-06-28 23:53:05 +0200

    resource_domain, resource_domain_record: migrate to public API

--- exoscale/resource_exoscale_domain_record.go
+++ exoscale/resource_exoscale_domain_record.go
@@ -2,11 +2,14 @@ package exoscale
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
-	"strconv"
 
-	"github.com/exoscale/egoscale"
+	exo "github.com/exoscale/egoscale/v2"
+	exoapi "github.com/exoscale/egoscale/v2/api"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
@@ -59,11 +62,11 @@ func resourceDomainRecord() *schema.Resource {
 			},
 		},
 
-		Create: resourceDomainRecordCreate,
-		Read:   resourceDomainRecordRead,
-		Update: resourceDomainRecordUpdate,
-		Delete: resourceDomainRecordDelete,
-		Exists: resourceDomainRecordExists,
+		CreateContext: resourceDomainRecordCreate,
+		ReadContext:   resourceDomainRecordRead,
+		UpdateContext: resourceDomainRecordUpdate,
+		DeleteContext: resourceDomainRecordDelete,
+		Exists:        resourceDomainRecordExists,
 
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
@@ -75,75 +78,148 @@ func resourceDomainRecord() *schema.Resource {
 			Update: schema.DefaultTimeout(defaultTimeout),
 			Delete: schema.DefaultTimeout(defaultTimeout),
 		},
+
+		SchemaVersion: 1,
+		StateUpgraders: []schema.StateUpgrader{
+			{
+				Type:    resourceDomainRecordV0().CoreConfigSchema().ImpliedType(),
+				Upgrade: resourceDomainRecordStateUpgradeV0,
+				Version: 0,
+			},
+		},
 	}
 }
 
-func resourceDomainRecordCreate(d *schema.ResourceData, meta interface{}) error {
-	log.Printf("[DEBUG] %s: beginning create", resourceDomainRecordIDString(d))
+func resourceDomainRecordV0() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type: schema.TypeString,
+			},
+			"domain": {
+				Type: schema.TypeString,
+			},
+			"record_type": {
+				Type: schema.TypeString,
+			},
+			"name": {
+				Type: schema.TypeString,
+			},
+			"content": {
+				Type: schema.TypeString,
+			},
+		},
+	}
+}
 
-	ctx, cancel := context.WithTimeout(context.Background(), d.Timeout(schema.TimeoutCreate))
-	defer cancel()
+func resourceDomainRecordStateUpgradeV0(
+	ctx context.Context,
+	rawState map[string]interface{},
+	meta interface{},
+) (map[string]interface{}, error) {
+	client := GetComputeClient(meta)
 
-	client := GetDNSClient(meta)
-
-	record, err := client.CreateRecord(ctx, d.Get("domain").(string), egoscale.DNSRecord{
-		Name:       d.Get("name").(string),
-		Content:    d.Get("content").(string),
-		RecordType: d.Get("record_type").(string),
-		TTL:        d.Get("ttl").(int),
-		Prio:       d.Get("prio").(int),
-	})
+	domainName := rawState["domain"].(string)
+	domains, err := client.ListDNSDomains(ctx, defaultZone)
 	if err != nil {
-		return err
+		return nil, fmt.Errorf("error retrieving domain list: %s", err)
 	}
 
-	d.SetId(strconv.FormatInt(record.ID, 10))
+	for _, domain := range domains {
+		if *domain.UnicodeName == domainName {
+			rawState["domain"] = *domain.ID
+			break
+		}
+	}
+
+	records, err := client.ListDNSDomainRecords(ctx, defaultZone, rawState["domain"].(string))
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving domain records: %q", err)
+	}
+
+	for _, record := range records {
+		if *record.Type == rawState["record_type"].(string) &&
+			*record.Name == rawState["name"].(string) &&
+			*record.Content == rawState["content"] {
+			rawState["id"] = *record.ID
+			break
+		}
+	}
+
+	return rawState, nil
+}
+
+func resourceDomainRecordCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	log.Printf("[DEBUG] %s: beginning create", resourceDomainRecordIDString(d))
+
+	ctx, cancel := context.WithTimeout(ctx, d.Timeout(schema.TimeoutRead))
+	defer cancel()
+
+	client := GetComputeClient(meta)
+
+	name := d.Get("name").(string)
+	content := d.Get("content").(string)
+	rtype := d.Get("record_type").(string)
+	t := d.Get("ttl").(int)
+	ttl := int64(t)
+	p := d.Get("prio").(int)
+	prio := int64(p)
+	record, err := client.CreateDNSDomainRecord(ctx, defaultZone, d.Get("domain").(string), &exo.DNSDomainRecord{
+		Name:     &name,
+		Content:  &content,
+		Type:     &rtype,
+		TTL:      &ttl,
+		Priority: &prio,
+	})
+	if err != nil {
+		return diag.Errorf("error creating domain record: %q", err)
+	}
+
+	d.SetId(*record.ID)
 
 	log.Printf("[DEBUG] %s: create finished successfully", resourceDomainRecordIDString(d))
 
-	return resourceDomainRecordRead(d, meta)
+	return resourceDomainRecordRead(ctx, d, meta)
 }
 
 func resourceDomainRecordExists(d *schema.ResourceData, meta interface{}) (bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), d.Timeout(schema.TimeoutRead))
 	defer cancel()
 
-	client := GetDNSClient(meta)
+	client := GetComputeClient(meta)
 
-	id, _ := strconv.ParseInt(d.Id(), 10, 64)
-	domain := d.Get("domain").(string)
+	domainID := d.Get("domain").(string)
 
-	if domain != "" {
-		record, err := client.GetRecord(ctx, domain, id)
+	if domainID != "" {
+		_, err := client.GetDNSDomainRecord(ctx, defaultZone, domainID, d.Id())
 		if err != nil {
-			if dnserr, ok := err.(*egoscale.DNSErrorResponse); ok && dnserr.Message == "Record not found" {
+			if errors.Is(err, exoapi.ErrNotFound) {
 				return false, nil
 			}
-
-			return true, fmt.Errorf("Failed to get DNS record id %d for domain %q from Exoscale API: %s", id, domain, err)
+			return false, err
 		}
 
-		return record != nil, nil
+		return true, nil
 	}
 
 	// If we reach this stage it means that we're in "import" mode, so we don't have the domain information yet.
 	// We have to scroll each existing domain's records and try to find one matching the resource ID.
 	log.Printf("[DEBUG] %s: import mode detected, trying to locate the record domain", resourceDomainRecordIDString(d))
 
-	domains, err := client.GetDomains(ctx)
+	domains, err := client.ListDNSDomains(ctx, defaultZone)
 	if err != nil {
 		return false, err
 	}
 
 	for _, domain := range domains {
-		records, err := client.GetRecords(ctx, domain.Name)
+		records, err := client.ListDNSDomainRecords(ctx, defaultZone, *domain.ID)
 		if err != nil {
 			return false, err
 		}
 
 		for _, record := range records {
-			if record.ID == id {
-				log.Printf("[DEBUG] %s: found record domain: %s", resourceDomainRecordIDString(d), domain.Name)
+			if *record.ID == d.Id() {
+				log.Printf("[DEBUG] %s: found record domain: %s", resourceDomainRecordIDString(d), *domain.UnicodeName)
 				return true, nil
 			}
 		}
@@ -152,95 +228,139 @@ func resourceDomainRecordExists(d *schema.ResourceData, meta interface{}) (bool,
 	return false, nil
 }
 
-func resourceDomainRecordRead(d *schema.ResourceData, meta interface{}) error {
+func resourceDomainRecordRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	log.Printf("[DEBUG] %s: beginning read", resourceDomainRecordIDString(d))
 
-	ctx, cancel := context.WithTimeout(context.Background(), d.Timeout(schema.TimeoutRead))
+	ctx, cancel := context.WithTimeout(ctx, d.Timeout(schema.TimeoutRead))
 	defer cancel()
 
-	client := GetDNSClient(meta)
+	client := GetComputeClient(meta)
 
-	id, _ := strconv.ParseInt(d.Id(), 10, 64)
-	domain := d.Get("domain").(string)
+	domainID := d.Get("domain").(string)
 
-	if domain != "" {
-		record, err := client.GetRecord(ctx, domain, id)
+	if domainID != "" {
+		domain, err := client.GetDNSDomain(ctx, defaultZone, domainID)
 		if err != nil {
-			return err
+			return diag.Errorf("error retrieving domain: %s", err)
+		}
+
+		record, err := client.GetDNSDomainRecord(ctx, defaultZone, domainID, d.Id())
+		if err != nil {
+			return diag.Errorf("error retrieving domain record: %s", err)
 		}
 
 		log.Printf("[DEBUG] %s: read finished successfully", resourceDomainRecordIDString(d))
 
-		return resourceDomainRecordApply(d, *record)
+		err = resourceDomainRecordApply(d, *domain.UnicodeName, record)
+		if err != nil {
+			return diag.Errorf("%s", err)
+		}
+
+		return nil
 	}
 
 	// If we reach this stage it means that we're in "import" mode, so we don't have the domain information yet.
 	// We have to scroll each existing domain's records and try to find one matching the resource ID.
 	log.Printf("[DEBUG] %s: import mode detected, trying to locate the record domain", resourceDomainRecordIDString(d))
 
-	domains, err := client.GetDomains(ctx)
+	domains, err := client.ListDNSDomains(ctx, defaultZone)
 	if err != nil {
-		return err
+		return diag.Errorf("error retrieving domains: %s", err)
 	}
 
 	for _, domain := range domains {
-		records, err := client.GetRecords(ctx, domain.Name)
+		records, err := client.ListDNSDomainRecords(ctx, defaultZone, *domain.ID)
 		if err != nil {
-			return err
+			return diag.Errorf("error retrieving domain records: %s", err)
 		}
 
 		for _, record := range records {
-			if record.ID == id {
-				if err := d.Set("domain", domain.Name); err != nil {
-					return err
+			if *record.ID == d.Id() {
+				if err := d.Set("domain", domain.ID); err != nil {
+					return diag.Errorf("%s", err)
 				}
 
 				log.Printf("[DEBUG] %s: read finished successfully", resourceDomainRecordIDString(d))
 
-				return resourceDomainRecordApply(d, record)
+				err = resourceDomainRecordApply(d, *domain.UnicodeName, &record)
+				if err != nil {
+					return diag.Errorf("%s", err)
+				}
+
+				return nil
 			}
 		}
 	}
 
-	return fmt.Errorf("domain record %s not found", d.Id())
+	return diag.Errorf("domain record %s not found", d.Id())
 }
 
-func resourceDomainRecordUpdate(d *schema.ResourceData, meta interface{}) error {
+func resourceDomainRecordUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	log.Printf("[DEBUG] %s: beginning update", resourceDomainRecordIDString(d))
 
-	ctx, cancel := context.WithTimeout(context.Background(), d.Timeout(schema.TimeoutUpdate))
+	ctx, cancel := context.WithTimeout(ctx, d.Timeout(schema.TimeoutRead))
 	defer cancel()
 
-	client := GetDNSClient(meta)
+	client := GetComputeClient(meta)
 
-	id, _ := strconv.ParseInt(d.Id(), 10, 64)
-	record, err := client.UpdateRecord(ctx, d.Get("domain").(string), egoscale.UpdateDNSRecord{
-		ID:      id,
-		Name:    d.Get("name").(string),
-		Content: d.Get("content").(string),
-		TTL:     d.Get("ttl").(int),
-		Prio:    d.Get("prio").(int),
+	name := d.Get("name").(string)
+	content := d.Get("content").(string)
+	rtype := d.Get("record_type").(string)
+	t := d.Get("ttl").(int)
+	ttl := int64(t)
+	p := d.Get("prio").(int)
+	prio := int64(p)
+	id := d.Id()
+	err := client.UpdateDNSDomainRecord(ctx, defaultZone, d.Get("domain").(string), &exo.DNSDomainRecord{
+		ID:       &id,
+		Name:     &name,
+		Content:  &content,
+		Type:     &rtype,
+		TTL:      &ttl,
+		Priority: &prio,
 	})
 	if err != nil {
-		return err
+		return diag.Errorf("error updating domain record: %s", err)
 	}
 
 	log.Printf("[DEBUG] %s: update finished successfully", resourceDomainRecordIDString(d))
 
-	return resourceDomainRecordApply(d, *record) // FIXME: use resourceDomainRecordRead()
+	domainID := d.Get("domain").(string)
+
+	domain, err := client.GetDNSDomain(ctx, defaultZone, domainID)
+	if err != nil {
+		return diag.Errorf("error retrieving domain: %s", err)
+	}
+
+	record, err := client.GetDNSDomainRecord(ctx, defaultZone, domainID, d.Id())
+	if err != nil {
+		return diag.Errorf("error retrieving domain record: %s", err)
+	}
+
+	err = resourceDomainRecordApply(d, *domain.UnicodeName, record) // FIXME: use resourceDomainRecordRead()
+	if err != nil {
+		return diag.Errorf("%s", err)
+	}
+
+	return nil
 }
 
-func resourceDomainRecordDelete(d *schema.ResourceData, meta interface{}) error {
+func resourceDomainRecordDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	log.Printf("[DEBUG] %s: beginning delete", resourceDomainRecordIDString(d))
 
-	ctx, cancel := context.WithTimeout(context.Background(), d.Timeout(schema.TimeoutDelete))
+	ctx, cancel := context.WithTimeout(ctx, d.Timeout(schema.TimeoutRead))
 	defer cancel()
 
-	client := GetDNSClient(meta)
+	client := GetComputeClient(meta)
 
-	id, _ := strconv.ParseInt(d.Id(), 10, 64)
-	if err := client.DeleteRecord(ctx, d.Get("domain").(string), id); err != nil {
-		return err
+	record, err := client.GetDNSDomainRecord(ctx, defaultZone, d.Get("domain").(string), d.Id())
+	if err != nil {
+		return diag.Errorf("error retrieving domain record: %s", err)
+	}
+
+	err = client.DeleteDNSDomainRecord(ctx, defaultZone, d.Get("domain").(string), record)
+	if err != nil {
+		return diag.Errorf("error deleting domain record: %s", err)
 	}
 
 	log.Printf("[DEBUG] %s: delete finished successfully", resourceDomainRecordIDString(d))
@@ -248,30 +368,30 @@ func resourceDomainRecordDelete(d *schema.ResourceData, meta interface{}) error 
 	return nil
 }
 
-func resourceDomainRecordApply(d *schema.ResourceData, record egoscale.DNSRecord) error {
-	d.SetId(strconv.FormatInt(record.ID, 10))
+func resourceDomainRecordApply(d *schema.ResourceData, domainName string, record *exo.DNSDomainRecord) error {
+	d.SetId(*record.ID)
 	if err := d.Set("name", record.Name); err != nil {
 		return err
 	}
 	if err := d.Set("content", record.Content); err != nil {
 		return err
 	}
-	if err := d.Set("record_type", record.RecordType); err != nil {
+	if err := d.Set("record_type", record.Type); err != nil {
 		return err
 	}
 	if err := d.Set("ttl", record.TTL); err != nil {
 		return err
 	}
-	if err := d.Set("prio", record.Prio); err != nil {
+	if err := d.Set("prio", record.Priority); err != nil {
 		return err
 	}
 
-	domain := d.Get("domain").(string)
-	if record.Name != "" {
-		domain = fmt.Sprintf("%s.%s", record.Name, domain)
+	hostname := domainName
+	if record.Name != nil && *record.Name != "" {
+		hostname = fmt.Sprintf("%s.%s", *record.Name, domainName)
 	}
 
-	if err := d.Set("hostname", domain); err != nil {
+	if err := d.Set("hostname", hostname); err != nil {
 		return nil
 	}
 
