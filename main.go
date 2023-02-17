ef11703d7aa44b5fcbb61546fcb8e37bf20d00f8
Philipp Sauter <sauterp@protonmail.com>
2023-02-17 13:36:11 +0100

    tooling: vendor tfplugindocs to generate documentation from code

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
+//go:generate terraform fmt -recursive ./examples/
+
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	var debugMode bool
 	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
