12a268a4ffdb47adb5c60592b82272d87d449102
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-02-21 17:50:30 +0100

    generate doc for instance_pool resource

---
+++ examples/resources/exoscale_instance_pool/resource.tf
@@ -0,0 +1,14 @@
+data "exoscale_compute_template" "my_template" {
+  zone = "ch-gva-2"
+  name = "Linux Ubuntu 22.04 LTS 64-bit"
+}
+
+resource "exoscale_instance_pool" "my_instance_pool" {
+  zone = "ch-gva-2"
+  name = "my-instance-pool"
+
+  template_id   = data.exoscale_compute_template.my_template.id
+  instance_type = "standard.medium"
+  disk_size     = 10
+  size          = 3
+}
