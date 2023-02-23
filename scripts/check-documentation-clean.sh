32b37588ce4ffba3a56c37f772604298d7561430
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-02-23 15:57:06 +0100

    show offending files in check-documentation-clean.sh output

--- scripts/check-documentation-clean.sh
+++ scripts/check-documentation-clean.sh
@@ -4,6 +4,7 @@ go generate
 if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
     echo "documentation is up to date"
 else
-    echo "documentation has not been updated"
+    echo "documentation has not been updated; offending files:"
+    git status --untracked-files=no --porcelain
     exit 1
 fi
