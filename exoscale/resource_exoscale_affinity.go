f34891a28e226be76971782ff4365c8aece8dd52
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-02-23 15:45:36 +0100

    test commit to verify that the CI step fails if the documentation checked into git does not match the generated output

--- exoscale/resource_exoscale_affinity.go
+++ exoscale/resource_exoscale_affinity.go
@@ -25,7 +25,7 @@ func resourceAffinity() *schema.Resource {
 				Type:        schema.TypeString,
 				Required:    true,
 				ForceNew:    true,
-				Description: "The anti-affinity group name.",
+				Description: "The anti-affinity group name. TEST",
 			},
 			"description": {
 				Type:        schema.TypeString,
