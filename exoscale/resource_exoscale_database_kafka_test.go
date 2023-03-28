a8fcd7bac20b14bab77c7e0a7c04413368e856e0
Predrag Janosevic <predrag.janosevic@exoscale.com>
2023-03-28 12:06:03 +0200

    Bump kafka version for testing

--- exoscale/resource_exoscale_database_kafka_test.go
+++ exoscale/resource_exoscale_database_kafka_test.go
@@ -14,7 +14,7 @@ import (
 
 var (
 	testAccResourceDatabaseKafkaIPFilter = []string{"1.2.3.4/32"}
-	testAccResourceDatabaseKafkaVersion  = "3.2"
+	testAccResourceDatabaseKafkaVersion  = "3.3"
 	testAccResourceDatabasePlanKafka     = "business-4"
 
 	testAccResourceDatabaseConfigCreateKafka = fmt.Sprintf(`
