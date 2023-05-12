a91601d48b5126dbc22d9456ec840cbb38a9687a
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-05-12 19:33:03 +0200

    lint

--- pkg/list/filterable_list_datasource.go
+++ pkg/list/filterable_list_datasource.go
@@ -3,11 +3,12 @@ package list
 import (
 	"context"
 
-	"github.com/exoscale/terraform-provider-exoscale/pkg/filter"
-	"github.com/exoscale/terraform-provider-exoscale/pkg/general"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/exoscale/terraform-provider-exoscale/pkg/filter"
+	"github.com/exoscale/terraform-provider-exoscale/pkg/general"
 )
 
 const (
