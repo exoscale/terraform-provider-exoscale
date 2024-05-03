a841341e9a3768866e113e1806bdaafe487e3e59
Philipp Sauter <philipp.sauter@exoscale.ch>
2024-05-03 15:34:04 +0200

    rename test files

--- pkg/resources/block_storage/main_test.go
+++ pkg/resources/block_storage/main_test.go
@@ -53,137 +53,137 @@ func TestBlockStorage(t *testing.T) {
 				),
 			},
 			// // 2 Update volume name only
-			// {
-			// 	Config: testutils.ParseTestdataConfig("./testdata/001.1.volume_rename.tf.tmpl", &testdataSpec),
-			// 	Check: resource.ComposeAggregateTestCheckFunc(
-			// 		resource.TestCheckResourceAttr(
-			// 			volumeResourceName,
-			// 			"name",
-			// 			fmt.Sprintf("terraform-provider-test-%d-renamed", testdataSpec.ID),
-			// 		),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "labels.%", "1"),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "labels.foo", "bar"),
-			// 		resource.TestCheckResourceAttrSet(volumeResourceName, "created_at"),
-			// 		resource.TestCheckResourceAttrSet(volumeResourceName, "blocksize"),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "state", "detached"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "size", "10"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "labels.%", "1"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo", "bar"),
-			// 		resource.TestCheckResourceAttrSet(volumeDataSourceName, "created_at"),
-			// 		resource.TestCheckResourceAttrSet(volumeDataSourceName, "blocksize"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "state", "detached"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "snapshots.#", "0"),
-			// 		resource.TestCheckNoResourceAttr(volumeDataSourceName, "instance"),
-			// 	),
-			// },
-			// // 3 Clear volume labels by setting labels attribute to empty
-			// {
-			// 	Config: testutils.ParseTestdataConfig("./testdata/001.3.volume_empty_labels.tf.tmpl", &testdataSpec),
-			// 	Check: resource.ComposeAggregateTestCheckFunc(
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "labels.%", "0"),
-			// 		resource.TestCheckResourceAttrSet(volumeResourceName, "created_at"),
-			// 		resource.TestCheckResourceAttrSet(volumeResourceName, "blocksize"),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "state", "detached"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "size", "10"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "labels.%", "0"),
-			// 		resource.TestCheckResourceAttrSet(volumeDataSourceName, "created_at"),
-			// 		resource.TestCheckResourceAttrSet(volumeDataSourceName, "blocksize"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "state", "detached"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "snapshots.#", "0"),
-			// 		resource.TestCheckNoResourceAttr(volumeDataSourceName, "instance"),
-			// 	),
-			// },
-			// // 4 Update volume lables only
-			// {
-			// 	Config: testutils.ParseTestdataConfig("./testdata/002.volume_update.tf.tmpl", &testdataSpec),
-			// 	Check: resource.ComposeAggregateTestCheckFunc(
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "labels.%", "2"),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "labels.foo1", "bar1"),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "labels.foo2", "bar2"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "labels.%", "2"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo1", "bar1"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo2", "bar2"),
-			// 	),
-			// },
-			// // 5 Clear volume labels by unsetting labels attribute
-			// {
-			// 	Config: testutils.ParseTestdataConfig("./testdata/001.2.volume_clear_labels.tf.tmpl", &testdataSpec),
-			// 	Check: resource.ComposeAggregateTestCheckFunc(
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "labels.%", "0"),
-			// 		resource.TestCheckResourceAttrSet(volumeResourceName, "created_at"),
-			// 		resource.TestCheckResourceAttrSet(volumeResourceName, "blocksize"),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "state", "detached"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "size", "10"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "labels.%", "0"),
-			// 		resource.TestCheckResourceAttrSet(volumeDataSourceName, "created_at"),
-			// 		resource.TestCheckResourceAttrSet(volumeDataSourceName, "blocksize"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "state", "detached"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "snapshots.#", "0"),
-			// 		resource.TestCheckNoResourceAttr(volumeDataSourceName, "instance"),
-			// 	),
-			// },
-			// // 6 Update volume labels and name
-			// {
-			// 	Config: testutils.ParseTestdataConfig("./testdata/002.2.volume_update_labels_and_name.tf.tmpl", &testdataSpec),
-			// 	Check: resource.ComposeAggregateTestCheckFunc(
-			// 		resource.TestCheckResourceAttr(
-			// 			volumeResourceName,
-			// 			"name",
-			// 			fmt.Sprintf("terraform-provider-test-%d-renamed-again", testdataSpec.ID),
-			// 		),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "labels.%", "3"),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "labels.foo1", "bar1"),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "labels.foo2", "bar2"),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "labels.foo3", "bar3"),
-			// 		resource.TestCheckResourceAttrSet(volumeResourceName, "created_at"),
-			// 		resource.TestCheckResourceAttrSet(volumeResourceName, "blocksize"),
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "state", "detached"),
-			// 		resource.TestCheckResourceAttr(
-			// 			volumeDataSourceName,
-			// 			"name",
-			// 			fmt.Sprintf("terraform-provider-test-%d-renamed-again", testdataSpec.ID),
-			// 		),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "size", "10"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "labels.%", "3"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo1", "bar1"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo2", "bar2"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo3", "bar3"),
-			// 		resource.TestCheckResourceAttrSet(volumeDataSourceName, "created_at"),
-			// 		resource.TestCheckResourceAttrSet(volumeDataSourceName, "blocksize"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "state", "detached"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "snapshots.#", "0"),
-			// 		resource.TestCheckNoResourceAttr(volumeDataSourceName, "instance"),
-			// 	),
-			// },
-			// // 7 Resize volume
-			// {
-			// 	Config: testutils.ParseTestdataConfig("./testdata/003.volume_resize.tf.tmpl", &testdataSpec),
-			// 	Check: resource.ComposeAggregateTestCheckFunc(
-			// 		resource.TestCheckResourceAttr(volumeResourceName, "size", "20"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "size", "20"),
-			// 	),
-			// },
-			// // 8 Create instance & attach volume
-			// {
-			// 	Config: testutils.ParseTestdataConfig("./testdata/004.volume_attach.tf.tmpl", &testdataSpec),
-			// 	Check: resource.ComposeAggregateTestCheckFunc(
-			// 		resource.TestCheckResourceAttr("exoscale_compute_instance.test_instance", "block_storage_volume_ids.#", "1"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "instance.%", "1"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "state", "attached"),
-			// 	),
-			// },
-			// // 9 Detach volume from instance
-			// {
-			// 	Config: testutils.ParseTestdataConfig("./testdata/005.volume_detach.tf.tmpl", &testdataSpec),
-			// 	Check: resource.ComposeAggregateTestCheckFunc(
-			// 		resource.TestCheckResourceAttr("exoscale_compute_instance.test_instance", "block_storage_volume_ids.#", "0"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "instance.%", "0"),
-			// 		resource.TestCheckResourceAttr(volumeDataSourceName, "state", "detached"),
-			// 	),
-			// },
+			{
+				Config: testutils.ParseTestdataConfig("./testdata/002.volume_rename.tf.tmpl", &testdataSpec),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						volumeResourceName,
+						"name",
+						fmt.Sprintf("terraform-provider-test-%d-renamed", testdataSpec.ID),
+					),
+					resource.TestCheckResourceAttr(volumeResourceName, "labels.%", "1"),
+					resource.TestCheckResourceAttr(volumeResourceName, "labels.foo", "bar"),
+					resource.TestCheckResourceAttrSet(volumeResourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(volumeResourceName, "blocksize"),
+					resource.TestCheckResourceAttr(volumeResourceName, "state", "detached"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "size", "10"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "labels.%", "1"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo", "bar"),
+					resource.TestCheckResourceAttrSet(volumeDataSourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(volumeDataSourceName, "blocksize"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "state", "detached"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "snapshots.#", "0"),
+					resource.TestCheckNoResourceAttr(volumeDataSourceName, "instance"),
+				),
+			},
+			// 3 Clear volume labels by setting labels attribute to empty
+			{
+				Config: testutils.ParseTestdataConfig("./testdata/003.volume_empty_labels.tf.tmpl", &testdataSpec),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(volumeResourceName, "labels.%", "0"),
+					resource.TestCheckResourceAttrSet(volumeResourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(volumeResourceName, "blocksize"),
+					resource.TestCheckResourceAttr(volumeResourceName, "state", "detached"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "size", "10"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "labels.%", "0"),
+					resource.TestCheckResourceAttrSet(volumeDataSourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(volumeDataSourceName, "blocksize"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "state", "detached"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "snapshots.#", "0"),
+					resource.TestCheckNoResourceAttr(volumeDataSourceName, "instance"),
+				),
+			},
+			// 4 Update volume lables only
+			{
+				Config: testutils.ParseTestdataConfig("./testdata/004.volume_update.tf.tmpl", &testdataSpec),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(volumeResourceName, "labels.%", "2"),
+					resource.TestCheckResourceAttr(volumeResourceName, "labels.foo1", "bar1"),
+					resource.TestCheckResourceAttr(volumeResourceName, "labels.foo2", "bar2"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "labels.%", "2"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo1", "bar1"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo2", "bar2"),
+				),
+			},
+			// 5 Clear volume labels by unsetting labels attribute
+			{
+				Config: testutils.ParseTestdataConfig("./testdata/005.volume_clear_labels.tf.tmpl", &testdataSpec),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(volumeResourceName, "labels.%", "0"),
+					resource.TestCheckResourceAttrSet(volumeResourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(volumeResourceName, "blocksize"),
+					resource.TestCheckResourceAttr(volumeResourceName, "state", "detached"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "size", "10"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "labels.%", "0"),
+					resource.TestCheckResourceAttrSet(volumeDataSourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(volumeDataSourceName, "blocksize"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "state", "detached"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "snapshots.#", "0"),
+					resource.TestCheckNoResourceAttr(volumeDataSourceName, "instance"),
+				),
+			},
+			// 6 Update volume labels and name
+			{
+				Config: testutils.ParseTestdataConfig("./testdata/006.volume_update_labels_and_name.tf.tmpl", &testdataSpec),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						volumeResourceName,
+						"name",
+						fmt.Sprintf("terraform-provider-test-%d-renamed-again", testdataSpec.ID),
+					),
+					resource.TestCheckResourceAttr(volumeResourceName, "labels.%", "3"),
+					resource.TestCheckResourceAttr(volumeResourceName, "labels.foo1", "bar1"),
+					resource.TestCheckResourceAttr(volumeResourceName, "labels.foo2", "bar2"),
+					resource.TestCheckResourceAttr(volumeResourceName, "labels.foo3", "bar3"),
+					resource.TestCheckResourceAttrSet(volumeResourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(volumeResourceName, "blocksize"),
+					resource.TestCheckResourceAttr(volumeResourceName, "state", "detached"),
+					resource.TestCheckResourceAttr(
+						volumeDataSourceName,
+						"name",
+						fmt.Sprintf("terraform-provider-test-%d-renamed-again", testdataSpec.ID),
+					),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "size", "10"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "labels.%", "3"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo1", "bar1"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo2", "bar2"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "labels.foo3", "bar3"),
+					resource.TestCheckResourceAttrSet(volumeDataSourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(volumeDataSourceName, "blocksize"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "state", "detached"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "snapshots.#", "0"),
+					resource.TestCheckNoResourceAttr(volumeDataSourceName, "instance"),
+				),
+			},
+			// 7 Resize volume
+			{
+				Config: testutils.ParseTestdataConfig("./testdata/007.volume_resize.tf.tmpl", &testdataSpec),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(volumeResourceName, "size", "20"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "size", "20"),
+				),
+			},
+			// 8 Create instance & attach volume
+			{
+				Config: testutils.ParseTestdataConfig("./testdata/008.volume_attach.tf.tmpl", &testdataSpec),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("exoscale_compute_instance.test_instance", "block_storage_volume_ids.#", "1"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "instance.%", "1"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "state", "attached"),
+				),
+			},
+			// 9 Detach volume from instance
+			{
+				Config: testutils.ParseTestdataConfig("./testdata/009.volume_detach.tf.tmpl", &testdataSpec),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("exoscale_compute_instance.test_instance", "block_storage_volume_ids.#", "0"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "instance.%", "0"),
+					resource.TestCheckResourceAttr(volumeDataSourceName, "state", "detached"),
+				),
+			},
 			// 10 Create snapshot
 			{
-				Config: testutils.ParseTestdataConfig("./testdata/006.create_snapshot.tf.tmpl", &testdataSpec),
+				Config: testutils.ParseTestdataConfig("./testdata/010.create_snapshot.tf.tmpl", &testdataSpec),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet(snapshotResourceName, "name"),
 					resource.TestCheckResourceAttrSet(snapshotResourceName, "size"),
@@ -201,7 +201,7 @@ func TestBlockStorage(t *testing.T) {
 			},
 			// 11 Create volume from snapshot
 			{
-				Config: testutils.ParseTestdataConfig("./testdata/007.volume_from_snapshot.tf.tmpl", &testdataSpec),
+				Config: testutils.ParseTestdataConfig("./testdata/011.volume_from_snapshot.tf.tmpl", &testdataSpec),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						volumeFromSnapshotResourceName,
@@ -216,7 +216,7 @@ func TestBlockStorage(t *testing.T) {
 			},
 			// 12 Clear snapshot labels by not setting labels attribute
 			{
-				Config: testutils.ParseTestdataConfig("./testdata/008.snapshot_clear_labels.tf.tmpl", &testdataSpec),
+				Config: testutils.ParseTestdataConfig("./testdata/012.snapshot_clear_labels.tf.tmpl", &testdataSpec),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet(snapshotResourceName, "name"),
 					resource.TestCheckResourceAttrSet(snapshotResourceName, "size"),
@@ -232,7 +232,7 @@ func TestBlockStorage(t *testing.T) {
 			},
 			// 13 Update snapshot name and labels
 			{
-				Config: testutils.ParseTestdataConfig("./testdata/009.snapshot_update_name_and_labels.tf.tmpl", &testdataSpec),
+				Config: testutils.ParseTestdataConfig("./testdata/013.snapshot_update_name_and_labels.tf.tmpl", &testdataSpec),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						snapshotResourceName,
@@ -261,7 +261,7 @@ func TestBlockStorage(t *testing.T) {
 			},
 			// 14 Update snapshot name only
 			{
-				Config: testutils.ParseTestdataConfig("./testdata/010.snapshot_update_name.tf.tmpl", &testdataSpec),
+				Config: testutils.ParseTestdataConfig("./testdata/014.snapshot_update_name.tf.tmpl", &testdataSpec),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						snapshotResourceName,
@@ -290,7 +290,7 @@ func TestBlockStorage(t *testing.T) {
 			},
 			// 15 Update snapshot labels only
 			{
-				Config: testutils.ParseTestdataConfig("./testdata/011.snapshot_update_labels.tf.tmpl", &testdataSpec),
+				Config: testutils.ParseTestdataConfig("./testdata/015.snapshot_update_labels.tf.tmpl", &testdataSpec),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						snapshotResourceName,
@@ -317,7 +317,7 @@ func TestBlockStorage(t *testing.T) {
 			},
 			// 16 Clear snapshot labels by setting empty labels attribute
 			{
-				Config: testutils.ParseTestdataConfig("./testdata/012.snapshot_empty_labels.tf.tmpl", &testdataSpec),
+				Config: testutils.ParseTestdataConfig("./testdata/016.snapshot_empty_labels.tf.tmpl", &testdataSpec),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						snapshotResourceName,
