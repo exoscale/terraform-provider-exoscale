664bd2e55ba9fe1c96f927ffecae4b91433ae61d
Philipp Sauter <philipp.sauter@exoscale.ch>
2024-06-14 10:43:52 +0200

    fix

--- exoscale/datasource_exoscale_template.go
+++ exoscale/datasource_exoscale_template.go
@@ -26,7 +26,7 @@ func dataSourceTemplate() *schema.Resource {
 	return &schema.Resource{
 		Description: `Fetch Exoscale [Compute Instance Templates](https://community.exoscale.com/documentation/compute/custom-templates/) data.
 
-Exoscale instance templates are regulary updated to include the latest updates. Whenever this happens, the template ID also changes which can lead terraform to plan the recreation of an instance. To work around this you may find [this issue](https://github.com/exoscale/terraform-provider-exoscale/issues/366) helpful.`,
+Exoscale instance templates are regularly updated to include the latest updates. Whenever this happens, the template ID also changes which can lead terraform to plan the recreation of an instance. To work around this you may find [this issue](https://github.com/exoscale/terraform-provider-exoscale/issues/366) helpful.`,
 		Schema: map[string]*schema.Schema{
 			dsTemplateAttrZone: {
 				Description: "The Exoscale [Zone](https://www.exoscale.com/datacenters/) name.",
