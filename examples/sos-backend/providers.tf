89a46e1519d3db04edba7d1dbf824f73f2378762
Arnaud Geiser <arnaudgeiser@gmail.com>
2024-09-04 13:56:28 +0200

    Remove skip_s3_checksum (#385)

--- examples/sos-backend/providers.tf
+++ examples/sos-backend/providers.tf
@@ -18,7 +18,6 @@ terraform {
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
-    skip_s3_checksum            = true
   }
 }
 
