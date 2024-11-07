f2c7d29f88ad77ca8593fee5f70ed2d98627c274
Philipp Sauter <philipp.sauter@exoscale.ch>
2024-11-07 20:37:36 +0100

    working acceptance test

--- pkg/resources/sos_bucket_policy/main_test.go
+++ pkg/resources/sos_bucket_policy/main_test.go
@@ -2,9 +2,11 @@ package sos_bucket_policy_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 	"time"
 
+	"github.com/hashicorp/terraform-plugin-testing/config"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 
 	"github.com/exoscale/terraform-provider-exoscale/pkg/testutils"
@@ -15,23 +17,38 @@ func TestSOSBucketPolicy(t *testing.T) {
 	// policyDataSourceName := "data." + policyResourceName
 
 	testdataSpec := testutils.TestdataSpec{
-		ID: time.Now().UnixNano(),
-		// Zone: testutils.TestZoneName,
-		Zone: "ch-gva-2",
+		ID:   time.Now().UnixNano(),
+		Zone: testutils.TestZoneName,
+	}
+
+	confVars := config.Variables{
+		"exoscale_api_key":    config.StringVariable(os.Getenv("EXOSCALE_API_KEY")),
+		"exoscale_api_secret": config.StringVariable(os.Getenv("EXOSCALE_API_SECRET")),
 	}
 
 	resource.Test(t, resource.TestCase{
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"aws": resource.ExternalProvider{
+				Source: "hashicorp/aws",
+			},
+		},
 		PreCheck:                 func() { testutils.AccPreCheck(t) },
 		ProtoV6ProviderFactories: testutils.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// 1 Create policy
 			{
-				Config: testutils.ParseTestdataConfig("./testdata/001.policy_create.tf.tmpl", &testdataSpec),
+				ConfigVariables: confVars,
+				Config:          testutils.ParseTestdataConfig("./testdata/001.policy_create.tf.tmpl", &testdataSpec),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						policyResourceName,
 						"bucket",
 						fmt.Sprintf("terraform-provider-test-%d", testdataSpec.ID),
+					),
+					resource.TestCheckResourceAttr(
+						policyResourceName,
+						"policy",
+						fmt.Sprintf("{\n  \"default-service-strategy\": \"allow\",\n  \"services\": {\n    \"sos\": {\n      \"type\": \"allow\"\n    }\n  }\n}\n"),
 					),
 				),
 			},
