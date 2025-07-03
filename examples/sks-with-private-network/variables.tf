cca09386f4ceaf661d3354bc395126a68550dd78
Arnoud Vermeer <arnoud.vermeer@exoscale.ch>
2025-07-03 11:06:07 +0200

    Clean up variables

--- examples/sks-with-private-network/variables.tf
+++ examples/sks-with-private-network/variables.tf
@@ -5,11 +5,3 @@ variable "exoscale_api_key" {
 variable "exoscale_secret_key" {
   default =  "api_secret_key_value_here"
 }
-
-variable "database_username" {
-  default =  "database_username_here"
-}
-
-variable "database_password" {
-  default =  "database_password_here"
-}
