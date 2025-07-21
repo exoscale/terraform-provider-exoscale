d3619cf10583ee181742b268d96d393ca01fd2a7
Arthur Aliiev <arthur.aliiev@exoscale.ch>
2025-07-21 15:27:37 +0200

    updates.

--- pkg/resources/instance/resource.go
+++ pkg/resources/instance/resource.go
@@ -57,6 +57,18 @@ func Resource() *schema.Resource {
 			Set:         schema.HashString,
 			Elem:        &schema.Schema{Type: schema.TypeString},
 		},
+		AttrEnableSecureBoot: {
+			Description: "Enable secure boot on the instancs (boolean; default: `false`).",
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Default:     false,
+		},
+		AttrEnableTPM: {
+			Description: "Enable TPM on the instance (boolean; default: `false`).",
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Default:     false,
+		},
 		AttrIPv6: {
 			Description: "Enable IPv6 on the instance (boolean; default: `false`).",
 			Type:        schema.TypeBool,
