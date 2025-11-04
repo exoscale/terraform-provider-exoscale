aa7a1692df8696a535a62d8f73222df9a0776472
Predrag Janosevic <predrag.janosevic@exoscale.com>
2025-11-04 12:24:48 +0000

    Fix SKS resource after breaking change in egoscale

--- exoscale/resource_exoscale_sks_cluster_test.go
+++ exoscale/resource_exoscale_sks_cluster_test.go
@@ -376,7 +376,7 @@ func TestAccResourceSKSCluster(t *testing.T) {
 
 						latestVersion := versions[0]
 
-						a.Equal(&egoscale.SKSClusterAddons{sksClusterAddonExoscaleCCM}, sksCluster.Addons)
+						a.Equal([]string{sksClusterAddonExoscaleCCM}, sksCluster.Addons)
 						a.True(defaultBool(sksCluster.AutoUpgrade, false))
 						a.Equal(defaultSKSClusterCNI, string(sksCluster.Cni))
 						a.Equal(testAccResourceSKSClusterDescription, sksCluster.Description)
