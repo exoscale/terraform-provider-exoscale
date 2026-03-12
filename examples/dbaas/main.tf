20fa0cd897f7b30b7ce5ed705204553a69fd1603
Simon Shillaker <simon.shillaker@exoscale.ch>
2026-03-12 14:59:06 +0100

    doc(dbaas): fix dbaas example

--- examples/dbaas/main.tf
+++ examples/dbaas/main.tf
@@ -8,7 +8,7 @@ resource "exoscale_dbaas" "postgres" {
   plan = "hobbyist-2"
   type = "pg"
   zone = local.my_zone
-  pg {
+  pg = {
     admin_username = var.database_username
     admin_password = var.database_password
     ip_filter      = ["0.0.0.0/0"]
