b06f127fa9cc8848e0cefacb7768423fad95685a
Natalie Perret <11332444+natalie-o-perret@users.noreply.github.com>
2026-04-24 12:44:12 +0200

    fix: bump terraform-plugin-testing to v1.16.0 and update dependencies

--- vendor/github.com/hashicorp/terraform-plugin-framework-validators/setvalidator/doc.go
+++ vendor/github.com/hashicorp/terraform-plugin-framework-validators/setvalidator/doc.go
@@ -1,2 +1,5 @@
-// Package setvalidator provides validators for types.Set attributes.
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+// Package setvalidator provides validators for types.Set attributes and function parameters.
 package setvalidator
