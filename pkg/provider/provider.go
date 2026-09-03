979b8b96c3a948cba5afb67b65cff9bb688e08e0
Philipp Sauter <philipp.sauter@exoscale.ch>
2026-09-03 18:18:24 +0000

    VPC integration: subnets

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -243,6 +243,7 @@ func (p *ExoscaleProvider) DataSources(ctx context.Context) []func() datasource.
 		domain.NewDataSource,
 		domain.NewDataSourceRecord,
 		vpc.NewDataSource,
+		vpc.NewDataSourceSubnet,
 	}
 }
 
@@ -278,6 +279,7 @@ func (p *ExoscaleProvider) Resources(ctx context.Context) []func() resource.Reso
 		domain.NewResource,
 		domain.NewResourceRecord,
 		vpc.NewResource,
+		vpc.NewResourceSubnet,
 		template.NewResourceTemplate,
 	}
 }
