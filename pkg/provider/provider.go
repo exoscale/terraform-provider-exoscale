f04cfa3832cec52e42a0f094d91c8810f2e6c0b0
Arthur Aliiev <arthur.aliiev@exoscale.ch>
2026-09-16 13:20:52 +0200

    formatting.

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -32,8 +32,8 @@ import (
 	privatenetwork "github.com/exoscale/terraform-provider-exoscale/pkg/resources/private_network"
 	"github.com/exoscale/terraform-provider-exoscale/pkg/resources/security_group"
 	"github.com/exoscale/terraform-provider-exoscale/pkg/resources/sos_bucket_policy"
-	"github.com/exoscale/terraform-provider-exoscale/pkg/resources/vpc"
 	"github.com/exoscale/terraform-provider-exoscale/pkg/resources/template"
+	"github.com/exoscale/terraform-provider-exoscale/pkg/resources/vpc"
 	"github.com/exoscale/terraform-provider-exoscale/pkg/resources/zones"
 	"github.com/exoscale/terraform-provider-exoscale/version"
 )
